fcc1f1bf3fe8149264d89accfea6bb4275b9c408
RaThomBoi <jessada.srm@gmail.com>
2022-06-19 22:32:37 +0700

    docs and feats : initialize Login.jsx and login logic

--- src/components/pages/auth/Login.jsx
+++ src/components/pages/auth/Login.jsx
@@ -1,47 +1,51 @@
 import React, {useState} from 'react'
+import { login } from '../../functions/auth'
 
-function Login() {
-    const [values, setValues] = useState({
-        username : '',
-        password : '',
-        password2: '',
-    })
+const Login = () => {
+
+  const [values, setValues] = useState({
+    username : '',
+    password : '',
+  })
+
+  const handleChange = e => {
+    setValues({...values, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();  
+
+    login(values)
+        .then(res=> {
+            console.log(res);
+            alert('Success');
+        }) 
+        .catch(err=>{
+            console.log(err.response);
+            alert(err.response.data);
+        })
     
-    const handleChange = e => {
-        setValues({...values, [e.target.name]: e.target.value })
+
     }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    
-        if(values.password !== values.password2) {
-            alert('Passwords do not match');
-            return;
-        } else {
-            
-        }
-    
-        }
-    
-        console.log('values: ', values);
-    return (
-    <div>
-        <h1>Login Page</h1>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="">username</label>
-            <input type="text" name='username' onChange={handleChange} />
-        
-            <label htmlFor="">password</label>
-            <input type="password" name='password' onChange={handleChange} />
-        
-            <label htmlFor="">confirm password</label>
-            <input type="password" name='password2' onChange={handleChange} />
-        
-            <button disabled={values.password.length < 6 } >Submit</button>
 
-        </form>
+  console.log('values: ', values);
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+
+        <label htmlFor="">username</label>
+        <input type="text" name='username' onChange={handleChange} />
+        
+        <label htmlFor="">password</label>
+        <input type="password" name='password' onChange={handleChange} />
+        
+        <button >Login</button>
+     
+      </form>
     </div>
   )
 }
 
-export default Login
+export default Login;
