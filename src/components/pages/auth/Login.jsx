9fca3e351dda7a5498242d600d74dc6d5b82c786
RaThomBoi <jessada.srm@gmail.com>
2022-06-20 00:35:45 +0700

    feat : implement singInAsync from authSlice in handle Login function

--- src/components/pages/auth/Login.jsx
+++ src/components/pages/auth/Login.jsx
@@ -1,51 +1,64 @@
 import React, {useState} from 'react'
 import { login } from '../../functions/auth'
 
+//redux
+import { useSelector, useDispatch } from 'react-redux'
+
+//middleware asyncThunk function
+import { signInAsync } from '../../../store/slices/authSlice'
+
 const Login = () => {
 
-  const [values, setValues] = useState({
-    username : '',
-    password : '',
-  })
+    const dispatch = useDispatch();
 
-  const handleChange = e => {
-    setValues({...values, [e.target.name]: e.target.value })
-  }
+    const [values, setValues] = useState({
+      username : '',
+      password : '',
+    })
 
-  const handleSubmit = (e) => {
-    e.preventDefault();  
+    const { loading, error} = useSelector(state => state.auth)
+    
+    const handleChange = e => {
+        setValues({...values, [e.target.name]: e.target.value })
+    }
 
-    login(values)
-        .then(res=> {
-            console.log(res);
-            alert('Success');
-        }) 
-        .catch(err=>{
-            console.log(err.response);
-            alert(err.response.data);
-        })
+    const handleSubmit = (e) => {
+        e.preventDefault();  
+
+        // login(values)
+        dispatch(signInAsync(values))
+            .then(res=> {
+                console.log(res);
+                alert('Success');
+                
+            }) 
+            .catch(err=>{
+                console.log(err.response);
+                // alert(err.response.data);
+                alert(err.response);
+            })
     
 
     }
 
-  console.log('values: ', values);
+    console.log('values: ', values);
 
-  return (
-    <div>
-      <h1>Login Page</h1>
-      <form onSubmit={handleSubmit}>
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <form onSubmit={handleSubmit}>
 
-        <label htmlFor="">username</label>
-        <input type="text" name='username' onChange={handleChange} />
-        
-        <label htmlFor="">password</label>
-        <input type="password" name='password' onChange={handleChange} />
-        
-        <button >Login</button>
-     
-      </form>
-    </div>
-  )
+                <label htmlFor="">username</label>
+                <input type="text" name='username' onChange={handleChange} />
+
+                <label htmlFor="">password</label>
+                <input type="password" name='password' onChange={handleChange} />
+
+                <button >Login</button>
+
+            </form>
+        </div>
+    )
 }
 
 export default Login;
