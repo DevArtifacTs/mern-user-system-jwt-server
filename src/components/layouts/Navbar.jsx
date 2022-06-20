0529e41f551c041bdf4917c0e29fa2499d8dea0e
RaThomBoi <jessada.srm@gmail.com>
2022-06-21 00:03:06 +0700

    feats : implement logout and navigate to '/' in Navbar.jsx

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -5,9 +5,29 @@ import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/ico
 import { Menu } from 'antd';
 
 //React Router
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+//redux
+import {useDispatch} from 'react-redux'; 
+
+//reducer
+import { signOut } from '../../store/slices/authSlice';
 
 function Navbar() {
+
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const redirectToSignIn = () => {
+    navigate('/');
+  }
+
+  const logOut = () => {
+    dispatch(signOut());
+    redirectToSignIn();
+  }
+  
   return (
     <Menu mode="horizontal" defaultSelectedKeys={['mail']}>
     
@@ -17,6 +37,10 @@ function Navbar() {
 
         <Menu.Item key="register" icon={<MailOutlined />}>
             <Link to='/register' >Register</Link>
+        </Menu.Item>    
+        
+        <Menu.Item onClick={logOut} key="logout" icon={<MailOutlined />}>
+            Logout
         </Menu.Item>    
     </Menu>
   )
