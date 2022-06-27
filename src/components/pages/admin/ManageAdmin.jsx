105e40e48b8d5296814d455cc9589690851616c8
RaThomBoi <jessada.srm@gmail.com>
2022-06-28 00:18:59 +0700

    feat : create handleRemove user and implement userDelete function from /functions

--- src/components/pages/admin/ManageAdmin.jsx
+++ src/components/pages/admin/ManageAdmin.jsx
@@ -5,10 +5,13 @@ import MenubarAdmin from '../../layouts/MenubarAdmin';
 import { useSelector } from 'react-redux';
 
 //Antd
-import { Switch } from 'antd';
+import { Switch, Select, Tag } from 'antd';
+
+//Antd Icons
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons' 
 
 //functions
-import {userList, changeStatus } from '../../functions/users'
+import {userList, changeStatus, changeUserRole, deleteUser } from '../../functions/users'
 
 function ManageAdmin(props) {
 
@@ -54,6 +57,45 @@ function ManageAdmin(props) {
             });
     }
 
+    const handleUserRoleChange = (e, id) => {
+        console.log('role = ', e);
+        console.log('_id = ', id);
+        const selectedUser = {
+            id : id,
+            role : e
+        }
+
+        changeUserRole(currentUser.token, selectedUser)
+            .then(res=>{
+                console.log(res)
+
+                //fetch data when request is success to make this page re-render again
+                loadData(currentUser.token);
+
+            }).catch(err=> {
+                console.log(err)
+            })
+    }
+
+    const handleRemove = (id) => {
+        if(window.confirm('Are you sure to delete this user?')){
+            console.log('id = ', id);
+        }
+            deleteUser(currentUser.token, id)
+                .then(res=>{
+                    console.log(res)
+                
+                    //fetch data when request is success to make this page re-render again
+                    loadData(currentUser.token);
+                
+                }).catch(err=> {
+                    console.log(err)
+                })
+    }
+
+
+    const roleData = ['admin', 'user'];
+
     return (
         <>
             <div className='container-fluid' >
@@ -72,6 +114,7 @@ function ManageAdmin(props) {
                                     <th scope="col">role</th>
                                     <th scope="col">created</th>
                                     <th scope="col">updated</th>
+                                    <th scope="col">Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -81,9 +124,29 @@ function ManageAdmin(props) {
                                             <th scope="row">{index + 1}</th>
                                             <td>{user.username}</td>
                                             <td><Switch checked={user.enabled} onChange={(e)=> handleChange(e, user._id)} /> </td>
-                                            <td>{user.role}</td>
+                                            <td>
+                                                <Select
+                                                    onChange={(e)=> handleUserRoleChange(e, user._id)}
+                                                    value={user.role} 
+                                                    style={{width:'100%'}} 
+                                                    >    
+                                                    {roleData.map((role, index)=> {
+                                                        return (
+                                                            <Select.Option key={index} value={role} >
+                                                                {role === 'admin'
+                                                                ? <Tag color='green' >{role}</Tag>
+                                                                : <Tag color='grey' >{role}</Tag>
+                                                                }
+                                                            </Select.Option>
+                                                        )
+                                                    })
+                                                    }
+                                                </Select>
+                                            </td>
                                             <td>{user.createdAt}</td>
                                             <td>{user.updatedAt}</td>
+                                            <td> <button onClick={()=> handleRemove(user._id)} style={{border:'0', background:'none'}} ><DeleteOutlined /></button> </td>
+                                            <td> <button onClick={()=> handleRemove(user._id)} style={{border:'0', background:'none'}} ><EditOutlined /></button> </td>
                                         </tr>
                                         )
                                 })
