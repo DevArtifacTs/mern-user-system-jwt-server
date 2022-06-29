491f5b6503ae2191217e7b5ac03bbe94f3c210e7
RaThomBoi <jessada.srm@gmail.com>
2022-06-30 00:38:53 +0700

    style, feats : 1.layout witrh Bootstrap form-control, form-text-float 2.implement toastify instaed of console.log and alert function to warn a user about of each page state (login successfully or failed, register successfullt or failed)

--- src/components/pages/auth/Login.jsx
+++ src/components/pages/auth/Login.jsx
@@ -10,7 +10,26 @@ import { signInAsync } from '../../../store/slices/authSlice'
 //useNavigate()
 import { useNavigate } from 'react-router-dom'
 
+//React Toastify
+import { toast } from 'react-toastify';
+
+//React Spinner
+import { CSSProperties } from "react";
+import DotLoader from "react-spinners/DotLoader";
+
+const override = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "blue",
+  };
+
+
+
 const Login = () => {
+
+    //React Spinner
+    // let [loading, setLoading] = useState(true);
+    let [color, setColor] = useState("#063ffa");
 
     const navigate = useNavigate();
 
@@ -37,21 +56,31 @@ const Login = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();  
-
+        
         dispatch(signInAsync(values))
             .then(res=> {
-                console.log(res);
-                alert('Success');
+                console.log('login response = ', res);
+                if(res.meta.requestStatus === "rejected"){
+                    toast.error(res.error.message);
+                } else{
+                    toast.info( ` welcome ${res.payload.username}, ${res.payload.role}` );
+                    localStorage.setItem('token', res.payload.token); 
+                    navigateBaseOnRole(res.payload.role);
+                }
+                
+                
+                // alert('Success');
                 //store token in local storage
-                localStorage.setItem('token', res.payload.token); 
-                console.log('token: ', res.payload.token)
-                console.log('role: ', res.payload.role)
-                navigateBaseOnRole(res.payload.role);
+                // console.log('token: ', res.payload.token)
+                // toast.info('token: ', res.payload.token)
+                // console.log('role: ', res.payload.role)
+                // toast.info('role: ', res.payload.role)
             }) 
             .catch(err=>{
                 console.log(err.response);
                 // alert(err.response.data);
-                alert(err.response);
+                // alert(err.response);
+                toast.error(err.response);
             })
         
     }
@@ -59,21 +88,47 @@ const Login = () => {
     // console.log('values: ', values);
 
     return (
-        <div>
-            <h1>Login Page</h1>
-            <form onSubmit={handleSubmit}>
+        <div className='container mt-5 p-5 ' style={{background:'#dbdbf3', maxWidth:'550px'}} >
+            <h1 className='' >Login</h1>
+            <div className="row">
+                <div className="col">
+                    <form onSubmit={handleSubmit}>
 
-                <label htmlFor="">username</label>
-                <input type="text" name='username' onChange={handleChange} />
+                        {/* <div className="form-group text-start"> */}
+                        <div className="form-floating ">
+                            <input 
+                                id='username'
+                                className='form-control' 
+                                type="text" 
+                                name='username' 
+                                onChange={handleChange} 
+                            />
+                            <label for="username">username</label>
+                        </div>
+                        <br />
+                        <div className="form-floating">
+                            <input 
+                                id='password'
+                                className='form-control' 
+                                type="password" 
+                                name='password' 
+                                onChange={handleChange} 
+                            />
+                            <label for="password">password</label>
+                        </div>
+                        {error 
+                            ? <p>{error}</p> 
+                            : <p></p>
+                        }
+                        <br />
+                        {loading
+                        ? <DotLoader color={color} loading={loading} cssOverride={override} size={30} />
+                        : <button className='btn btn-success' >{loading? 'Loading...' : 'Login' }</button>
+                        }
+                    </form>
 
-                <label htmlFor="">password</label>
-                <input type="password" name='password' onChange={handleChange} />
-                {error ? 
-                    <button >{error}</button> 
-                    :<button >{loading? 'Loading...' : 'Login' }</button>
-                }
-                
-            </form>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/pages/auth/Register.jsx
+++ src/components/pages/auth/Register.jsx
@@ -1,13 +1,20 @@
 import React, {useState} from 'react'
 import { register } from '../../functions/auth'
 
+//React Toastify
+import { toast } from 'react-toastify';
+
 const Register = () => {
+
+  const notify = () => toast("Register Successfully");
 
   const [values, setValues] = useState({
     username : '',
     password : '',
     password2: '',
   })
+
+  // const [error, setError] = useState('')
 
   const handleChange = e => {
     setValues({...values, [e.target.name]: e.target.value })
@@ -17,19 +24,23 @@ const Register = () => {
     e.preventDefault();
 
     if(values.password !== values.password2) {
-      alert('Passwords do not match');
+      // setError('Password does not match')
+      toast.error('Passwords do not match');
       return;
     } else {
-      alert('Success');
+      // alert('Success');
       
       //send request to server
       register(values)
         .then(res=> {
           console.log(res);
+          toast.success('Register Successfully');
+          // notify();
         }) 
         .catch(err=>{
           console.log(err.response);
-          alert(err.response.data);
+          // alert(err.response.data);
+          toast.error(err.response.data);
         })
     }
 
@@ -38,22 +49,56 @@ const Register = () => {
   console.log('values: ', values);
 
   return (
-    <div>
-      <h1>Register Page</h1>
-      <form onSubmit={handleSubmit}>
+    <div className='container mt-5 p-5 ' style={{background:'#dbdbf3', maxWidth:'550px'}} >
+      <h1>Register</h1>
+      <div className="row">
+                <div className="col">
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-floating ">
+                          <input 
+                            id='username'
+                            className='form-control'
+                            type="text" 
+                            name='username' 
+                            onChange={handleChange} 
+                          />
+                          <label for="username" >username</label>
+                        </div>
+                        <br />
+                        <div className="form-floating">
+                            <input 
+                                id='password'
+                                className='form-control' 
+                                type="password" 
+                                name='password' 
+                                onChange={handleChange} 
+                            />
+                            <label for="password">password</label>
+                        </div>
+                        <br />
+                        <div className="form-floating">
+                            <input 
+                                id='password'
+                                className='form-control' 
+                                type="password" 
+                                name='password2' 
+                                onChange={handleChange} 
+                            />
+                            <label for="password">confirm password</label>
+                            {/* {error &&  <p className='text-danger'>{error}</p>} */}
+                        </div>
+                        <br/>
+                        <button 
+                          className='btn btn-primary'
+                          disabled={values.password.length < 6 } 
+                        >
+                          Submit
+                        </button>
 
-        <label htmlFor="">username</label>
-        <input type="text" name='username' onChange={handleChange} />
-        
-        <label htmlFor="">password</label>
-        <input type="password" name='password' onChange={handleChange} />
-        
-        <label htmlFor="">confirm password</label>
-        <input type="password" name='password2' onChange={handleChange} />
+                    </form>
 
-        <button disabled={values.password.length < 6 } >Submit</button>
-     
-      </form>
+                </div>
+            </div>
     </div>
   )
 }
