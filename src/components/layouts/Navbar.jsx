dbad17cdfbfd7ce67febcfd7abe320d6d844902c
RaThomBoi <jessada.srm@gmail.com>
2022-06-30 00:35:16 +0700

    feats : 1.layout by Bootstrap 2.conditional rendering eg. if logined , then hide register and show logout menu instead

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
 //Antd
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { 
+  HomeOutlined, 
+  LoginOutlined, 
+  UserAddOutlined,
+  PoweroffOutlined } from '@ant-design/icons';
+
 import { Menu } from 'antd';
 
 //React Router
 import { Link, useNavigate } from 'react-router-dom';
 
 //redux
-import {useDispatch} from 'react-redux'; 
+import {useDispatch, useSelector} from 'react-redux'; 
 
 //reducer
 import { signOut } from '../../store/slices/authSlice';
@@ -18,6 +23,9 @@ function Navbar() {
   const navigate = useNavigate();
 
   const dispatch = useDispatch();
+
+  const { role, username } = useSelector(state => state.auth);
+  console.log('currentUser\'s role : ', role);
 
   const redirectToSignIn = () => {
     navigate('/');
@@ -29,19 +37,48 @@ function Navbar() {
   }
   
   return (
-    <Menu mode="horizontal" defaultSelectedKeys={['mail']}>
-    
-        <Menu.Item key="login" icon={<MailOutlined />}>
-            <Link to='/login' >Login</Link>
-        </Menu.Item>
+    <Menu mode="horizontal"   >
+      <div className="container-fluid">
+        <div className="row d-flex justify-content-end ">
+          <div className="col-6 d-flex justify-content-start ">
+            <Menu.Item  key="login" icon={<HomeOutlined />} >
+                <Link to={role === 'admin' ? '/admin/home' :  '/user/home' } >Home</Link>
+            </Menu.Item>
+          </div>
 
-        <Menu.Item key="register" icon={<MailOutlined />}>
-            <Link to='/register' >Register</Link>
-        </Menu.Item>    
+          {username
+          ? 
+            <>
+              <div className="col-3 text-center"> 
+                <button type="button" class="btn btn-primary">
+                Welcome Back <span class="badge bg-secondary">{username }</span>
+                </button>
+              </div>
+              <div className="col-2">
+                <Menu.Item onClick={logOut} key="logout" icon={<PoweroffOutlined />}>
+                    Logout
+                </Menu.Item>    
+              </div>
+            </>
+          : 
+            <>
+              <div className="col-2">
+                <Menu.Item  key="login" icon={<LoginOutlined />} >
+                    <Link to='/login' >Login</Link>
+                </Menu.Item> 
+              </div>
+              <div className="col-2">
+                <Menu.Item key="register" icon={<UserAddOutlined />}>
+                    <Link to='/register' >Register</Link>
+                </Menu.Item>    
+              </div>
+            </>
+          }
+          
+        </div>
+      </div>
+
         
-        <Menu.Item onClick={logOut} key="logout" icon={<MailOutlined />}>
-            Logout
-        </Menu.Item>    
     </Menu>
   )
 }
